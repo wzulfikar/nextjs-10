331a56a15a2bb47e5a60d5776584af324b7a3cd6
wzulfikar <me@wzulfikar.com>
2021-03-26 12:36:25 +0700

    chore(styles): install Tailwind v2

---
+++ pages/_preview/tailwind.tsx
@@ -0,0 +1,9 @@
+export default function TailwindPreview() {
+  return (
+    <div className="flex w-screen justify-center mt-10 text-2xl font-extrabold">
+      <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
+        Hello from Tailwind v2!
+      </span>
+    </div>
+  );
+}
