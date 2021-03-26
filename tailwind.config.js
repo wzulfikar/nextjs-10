1e0d48cf32fe33941788d0c876d3ac6ea98ec9f6
wzulfikar <me@wzulfikar.com>
2021-03-26 14:27:50 +0700

    chore(tailwind): disable preflight to prevent style conflict with ant design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {},
+  corePlugins: {
+    // Disable tailwind preflight to prevent conflict with ant design base styles.
+    // See: https://tailwindcss.com/docs/preflight#disabling-preflight
+    preflight: false,
+   }
 };
