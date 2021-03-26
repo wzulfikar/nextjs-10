4a659ac67dffcb1f2d366f1e45288860916a7b2f
wzulfikar <me@wzulfikar.com>
2021-03-26 12:40:51 +0700

    chore(tailwind): use tailwind jit

--- pages/_preview/tailwind.tsx
+++ pages/_preview/tailwind.tsx
@@ -1,6 +1,6 @@
 export default function TailwindPreview() {
   return (
-    <div className="flex w-screen justify-center mt-10 text-2xl font-extrabold">
+    <div className="flex w-screen justify-center text-2xl font-extrabold mt-[2rem]">
       <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
         Hello from Tailwind v2!
       </span>
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}", 
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {},
+};
