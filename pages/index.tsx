e55cc7e7c1cbce1846c3eaab5ffc43677ab23556
wzulfikar <me@wzulfikar.com>
2021-03-28 17:29:48 +0700

    chore(landingpage): move next.js official sections to secondary

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,29 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { DiGithubBadge } from 'react-icons/di';
 
 import styles from '@src/styles/Home.module.css';
 import ToggleDarkMode from '@src/components/ToggleDarkMode';
 import { trackGoal, events } from '@src/lib/analytic';
+import getGithubUrl from '@src/lib/github/getGithubUrl';
 
-const utm =
-  'utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app';
+const githubRepo = process.env.NEXT_PUBLIC_GITHUB_REPO;
+const repoParts = githubRepo.split('/');
+const repoName = repoParts.pop();
+const owner = repoParts.pop();
+
+const githubUrl = getGithubUrl({
+  owner,
+  repo: repoName,
+  branch: 'main',
+  file: 'pages/index.tsx',
+});
 
 export default function Home() {
   return (
     <div className={`${styles.container} dark:bg-[#111] vercel`}>
       <Head>
-        <title>Create Next App</title>
+        <title>Next.js 10 Template</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -24,14 +35,56 @@ export default function Home() {
 
         <p className={styles.description}>
           <span className="dark:text-white">Get started by editing </span>
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>
+            <a
+              title="Open pages/index.tsx in Github"
+              className="hover:underline"
+              href={githubUrl}
+              onClick={() => trackGoal(events.OpenGithubRepo)}
+            >
+              pages/index.tsx
+              <DiGithubBadge className="ml-1 h-6 w-6 inline relative -top-0.5" />
+            </a>
+          </code>
         </p>
 
-        <div className="-mb-10">
+        <div className="mt-3 -mb-9">
           <ToggleDarkMode />
         </div>
 
         <div className={`${styles.grid} dark:text-white`}>
+          <Link href={`/_preview`}>
+            <a className={styles.card}>
+              <h3>Preview &rarr;</h3>
+              <p>
+                Preview sample components at <code>/_preview</code>.
+              </p>
+            </a>
+          </Link>
+
+          <a
+            href={`/analytic`}
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h3>Analytic &rarr;</h3>
+            <p>View analytic dashboard for this Next.js template.</p>
+          </a>
+        </div>
+
+        <div
+          className={
+            'dark:text-white font-semibold  w-full flex justify-center sm:justify-start sm:pl-12 mt-2'
+          }
+        >
+          <p>More from Next.js official:</p>
+        </div>
+
+        <div
+          className={`${styles.grid} dark:text-white`}
+          style={{ marginTop: 0 }}
+        >
           <a href="https://nextjs.org/docs" className={styles.card}>
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
@@ -58,47 +111,6 @@ export default function Home() {
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
-        </div>
-
-        <div
-          className={
-            'dark:text-white font-semibold flex justify-start w-full px-4 sm:px-7 mt-2'
-          }
-        >
-          <p>
-            More from{' '}
-            <a
-              className="underline"
-              onClick={() => trackGoal(events.OpenGithubRepo)}
-              href="/github"
-            >
-              this template:
-            </a>
-          </p>
-        </div>
-
-        <div
-          className={`${styles.grid} dark:text-white`}
-          style={{ marginTop: 0 }}
-        >
-          <Link href={`/_preview?${utm}`}>
-            <a className={styles.card}>
-              <h3>Preview &rarr;</h3>
-              <p>
-                Preview sample components at <code>/_preview</code>.
-              </p>
-            </a>
-          </Link>
-
-          <a
-            href={`/analytic?${utm}`}
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h3>Analytic &rarr;</h3>
-            <p>View shared analytic dashboard for this Next.js template.</p>
           </a>
         </div>
       </main>
