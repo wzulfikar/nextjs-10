5bca456a71c5938f1b249a8baa643d27b50433b6
wzulfikar <me@wzulfikar.com>
2021-03-28 15:29:51 +0700

    fix(styles): disable style option from babel-plugin-import to manually control the order of stylesheet imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
-import '@src/styles/globals.css';
-
-import useAnalytic from '@src/lib/analytic/nextjs-hook';
-
 // Use `require` instead of `import` to include styles from less files.
 // Otherwise the styles will be applied in `next dev` but not in production.
 // See: https://github.com/SolidZORO/next-plugin-antd-less#tips
+require('@src/styles/antd.less');
 require('@src/styles/tailwind-antd-compat.less');
+
+// NOTE: somehow the styles won't be applied if using `import`
+require('@src/styles/globals.css');
+
+import useAnalytic from '@src/lib/analytic/nextjs-hook';
 
 function MyApp({ Component, pageProps }) {
   useAnalytic();
