7f9fe6f35fc29e4dbb5ceaa804100e5fa3cff792
wzulfikar <me@wzulfikar.com>
2021-03-28 10:51:39 +0700

    fix(styles): fix less styles not applied in production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import '@src/styles/globals.css';
-import '@src/styles/tailwind-antd-compat.less';
 
 import useAnalytic from '@src/lib/analytic/nextjs-hook';
+
+// Use `require` instead of `import` to include styles from less files.
+// Otherwise the styles will be applied in `next dev` but not in production.
+// See: https://github.com/SolidZORO/next-plugin-antd-less#tips
+require('@src/styles/tailwind-antd-compat.less');
 
 function MyApp({ Component, pageProps }) {
   useAnalytic();
